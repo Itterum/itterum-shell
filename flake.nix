4d065f117cf4c7257b5cd6baab7ce00a9f46e723
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2026-09-14 14:31:08 +0300

    ttt

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My Quickshell Niri setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    quickshell.url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+    quickshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, quickshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Собираем все Qt зависимости в один список
+        qtDeps = with pkgs.qt6; [
+          qtdeclarative
+          qtwayland
+          qt5compat
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            quickshell.packages.${system}.default
+          ] ++ qtDeps;
+
+          shellHook = ''
+            # 1. Указываем Qt, где лежат QML-модули и плагины (исправляет ошибку Qt5Compat)
+            export QML2_IMPORT_PATH="${pkgs.lib.makeSearchPath pkgs.qt6.qtbase.qtQmlPrefix qtDeps}"
+            export QT_PLUGIN_PATH="${pkgs.lib.makeSearchPath pkgs.qt6.qtbase.qtPluginPrefix qtDeps}"
+
+            # 2. Отключаем аппаратное ускорение OpenGL (исправляет ошибку EGL not available)
+            export QT_QUICK_BACKEND=software
+            export LIBGL_ALWAYS_SOFTWARE=1
+          '';
+        };
+      }
+    );
+}
